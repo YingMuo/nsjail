f5e3a0333d44bfdc65428967ae378fd09abd7459
YingMuo <wl03452329@gmail.com>
2021-08-22 15:03:54 +0800

    Add build.sh
    Add build.sh for first building this project.
    I will use apt to install some essential package.
    Need sudoer previlege to use it.

---
+++ build.sh
@@ -0,0 +1,7 @@
+sudo apt install pkg-config
+sudo apt install flex
+sudo apt install bison
+sudo apt install protobuf-compiler
+sudo apt install libnl-3-dev
+sudo apt install libnl-route-3-dev
+make
